7fdb331fdc37efa7c2b98c009ae8cb043c1b17bd
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-04-16 15:20:49 +0600

    dropdown 2 removed

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,7 +1,7 @@
 /*=========== taggle menu start ===========*/
 let TaggleMenuItems = document.querySelector('.taggle-menu-items');
 let TaggleMenuSubItems1 = document.querySelector('.dripdown1');
-let TaggleMenuSubItems2 = document.querySelector('.dripdown2');
+
 
 
 document.querySelector('#taggle-bar').onclick = () => {
@@ -13,9 +13,7 @@ document.querySelector('#dripdown1icon').onclick = () => {
     TaggleMenuSubItems1.classList.toggle('active');
 }
 
-document.querySelector('#dripdown2icon').onclick = () => {
-    TaggleMenuSubItems2.classList.toggle('active');
-}
+
 
 /*=========== taggle menu end ===========*/
 
